955a9c2d6719245e393e491dc0b2072baf1e4ed2
manalen <61988178+manalen@users.noreply.github.com>
2020-05-26 02:20:39 +0100

    Add files via upload

---
+++ Compenents/PatientService.js
@@ -0,0 +1,31 @@
+import axios from 'axios' 
+
+
+
+class PatientService {
+
+    
+    hola() {
+        return axios.get(`http://localhost:5000/med/showallpatient`);
+    }
+    bola(id ){
+        
+        return axios.get( `http://localhost:5000/med/showpatientbyid/${id}`);
+    }
+    cinola(cin){
+        return axios.get( `http://localhost:5000/med/showpatientbycin/${cin}`);
+    }
+    kola(id){
+        return axios.get(`http://localhost:8087/medecin/afficherprescriptionpatient/${id}` );
+    } 
+    ola( kaman){
+        
+        return axios.get(`http://localhost:5000/med/showpatientbyid/${kaman}`);
+
+    }
+    pola( consultation){
+    return axios.post(`http://localhost:8087/medecin/creerconsultation`, consultation);}
+
+}
+
+export default new PatientService()
